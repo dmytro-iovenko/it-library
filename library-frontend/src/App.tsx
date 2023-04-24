3d0e7e32e658ebc972c2f7e4b11688448c0add69
Dmytro Iovenko <iovenko@gmail.com>
2023-04-24 19:08:00 -0400

    creating carousel component

--- library-frontend/src/App.tsx
+++ library-frontend/src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Navbar } from "./layouts/NavbarAndFooter/Navbar";
 import { NewBooks } from "./layouts/HomePage/NewBooks";
+import { Carousel } from "./layouts/HomePage/Carousel";
 
 function App() {
   return (
     <>
       <Navbar />
       <NewBooks />
+      <Carousel />
     </>
   );
 }
