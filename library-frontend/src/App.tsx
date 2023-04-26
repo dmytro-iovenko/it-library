91e62ed2439b36503d7d816fa791a99fa6fa7f98
Dmytro Iovenko <iovenko@gmail.com>
2023-04-25 20:47:04 -0400

    creating library services component

--- library-frontend/src/App.tsx
+++ library-frontend/src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Navbar } from "./layouts/NavbarAndFooter/Navbar";
 import { NewBooks } from "./layouts/HomePage/NewBooks";
 import { Carousel } from "./layouts/HomePage/Carousel";
+import { LibraryServices } from "./layouts/HomePage/LibraryServices";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <NewBooks />
       <Carousel />
+      <LibraryServices />
     </>
   );
 }
