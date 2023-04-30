90130886ae2021238a416b4cc44a825b49edd48b
Dmytro Iovenko <iovenko@gmail.com>
2023-04-29 20:31:59 -0400

    styling

--- library-frontend/src/App.tsx
+++ library-frontend/src/App.tsx
@@ -7,14 +7,21 @@ import { SearchBooksPage } from "./layouts/SearchBooksPage/SearchBooksPage";
 export const App = () => {
   return (
     <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Navigate replace to='/home' />} />
-        <Route path="index.html" element={<Navigate replace to='/home' />} />
-        <Route path="/home" element={<HomePage />} />
-        <Route path="search" element={<SearchBooksPage />} />
-      </Routes>
-      <Footer />
+      <div className="d-flex flex-column min-vh-100">
+        <Navbar />
+        <div className="flex-grow-1">
+          <Routes>
+            <Route path="/" element={<Navigate replace to="/home" />} />
+            <Route
+              path="index.html"
+              element={<Navigate replace to="/home" />}
+            />
+            <Route path="/home" element={<HomePage />} />
+            <Route path="search" element={<SearchBooksPage />} />
+          </Routes>
+        </div>
+        <Footer />
+      </div>
     </BrowserRouter>
   );
 };
