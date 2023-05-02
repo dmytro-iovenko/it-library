e54f45fe8d61a14854c278856afece95a87e469a
Dmytro Iovenko <iovenko@gmail.com>
2023-05-01 22:34:35 -0400

    adding useEffect to book checkout page

--- library-frontend/src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
+++ library-frontend/src/layouts/BookCheckoutPage/BookCheckoutPage.tsx
@@ -1,12 +1,36 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { BookModel } from "../../models/BookModel";
+import { SpinnerLoading } from "../Utils/SpinnerLoading";
+import * as BooksAPI from "../../services/itbooks-api";
 
 export const BookCheckoutPage = () => {
-    const [book, setBook] = useState<BookModel>();
-    const [isLoading, setIsLoading] = useState(true);
-    const [httpError, setHttpError] = useState(null);
-    const { isbn } = useParams();
+  const [book, setBook] = useState<BookModel>();
+  const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState(null);
+  const { isbn } = useParams();
 
-    return(<div></div>);
-}
+  useEffect(() => {
+    isbn &&
+      BooksAPI.getBookByISBN(isbn)
+        .then((resultData: any) => setBook(resultData))
+        .catch((error: any) => {
+          setHttpError(error.message);
+        });
+    setIsLoading(false);
+  }, [isbn]);
+
+  if (isLoading) {
+    return <SpinnerLoading />;
+  }
+
+  if (httpError) {
+    return (
+      <div className="container m-5">
+        <p>{httpError}</p>
+      </div>
+    );
+  }
+
+  return <div></div>;
+};
