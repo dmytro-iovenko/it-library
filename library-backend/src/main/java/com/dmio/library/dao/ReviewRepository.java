fee3677410c81ede0c6b10b73963f9e832eb4173
Dmytro Iovenko <iovenko@gmail.com>
2023-05-03 22:35:35 -0400

    adding find review by isbn

--- library-backend/src/main/java/com/dmio/library/dao/ReviewRepository.java
+++ library-backend/src/main/java/com/dmio/library/dao/ReviewRepository.java
@@ -1,9 +1,12 @@
 package com.dmio.library.dao;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.dmio.library.entity.Review;
 
-public interface ReviewRepository extends JpaRepository<Review, Long>{
-    
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+    Page<Review> findByIsbn(@RequestParam("isbn") String isbn, Pageable pageable);
 }
