a140cd92eef4d813910085b7e83a8413853732d0
Dmytro Iovenko <iovenko@gmail.com>
2023-05-06 16:29:06 -0400

    adding login widget

---
+++ library-frontend/src/auth/LoginWidget.jsx
@@ -0,0 +1,31 @@
+import { useOktaAuth } from "@okta/okta-react";
+import { SpinnerLoading } from "../layouts/Utils/SpinnerLoading";
+
+const LoginWidget = ({ config }) => {
+  const { oktaAuth, authState } = useOktaAuth();
+  const onSuccess = (tokens) => {
+    oktaAuth.handleLoginRedirect(tokens);
+  };
+
+  const onError = (err) => {
+    console.log("error logging in", err);
+  };
+
+  if (!authState) {
+    return <SpinnerLoading />;
+  }
+
+  return (
+    <>
+      {authState.isAuthenticated && (
+        <OktaSignInWidget
+          config={config}
+          onSuccess={onSuccess}
+          onError={onError}
+        />
+      )}
+    </>
+  );
+};
+
+export default LoginWidget;
