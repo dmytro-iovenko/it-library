89e010a23ca7e98e2a9d1180b58e79b69f76b826
Dmytro Iovenko <iovenko@gmail.com>
2023-05-08 21:20:28 -0400

    adding checkout entity

---
+++ library-backend/src/main/java/com/dmio/library/entity/Checkout.java
@@ -0,0 +1,37 @@
+package com.dmio.library.entity;
+
+import javax.persistence.*;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="checkout")
+@Data
+@NoArgsConstructor
+public class Checkout {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "user_email")
+    private String userEmail;
+
+    @Column(name = "isbn")
+    private String isbn;
+
+    @Column(name = "checkout_date")
+    private String checkoutDate;
+
+    @Column(name = "return_date")
+    private String returnDate;
+
+    public Checkout(String userEmail, String isbn, String checkoutDate, String returnDate) {
+		this.userEmail = userEmail;
+		this.isbn = isbn;
+		this.checkoutDate = checkoutDate;
+		this.returnDate = returnDate;
+	}
+
+}
