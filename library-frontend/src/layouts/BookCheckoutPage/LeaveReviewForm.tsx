c5a3d35f88857ea9805b36a80be68fe5ae599722
Dmytro Iovenko <iovenko@gmail.com>
2023-05-16 20:36:28 -0400

    adding optional description to the form

--- library-frontend/src/layouts/BookCheckoutPage/LeaveReviewForm.tsx
+++ library-frontend/src/layouts/BookCheckoutPage/LeaveReviewForm.tsx
@@ -3,9 +3,12 @@ import { StarsReview } from "../Utils/StarsReview";
 
 export const LeaveReviewForm: React.FC<{}> = (props) => {
   const [starInput, setStarInput] = useState(0);
+  const [displayInput, setDisplayInput] = useState(false);
+  const [reviewDescription, setReviewDescription] = useState("");
 
   const starValue = (value: number) => {
     setStarInput(value);
+    setDisplayInput(true);
   };
 
   return (
@@ -31,6 +34,27 @@ export const LeaveReviewForm: React.FC<{}> = (props) => {
         ))}
       </ul>
       <StarsReview rating={starInput} size={32} />
+
+      {displayInput && (
+        <form method="POST" action="#">
+          <hr />
+          <div className="mb-3">
+            <label className="form-label">Description</label>
+            <textarea
+              className="form-control"
+              id="submitReviewDescription"
+              placeholder="Optional"
+              rows={3}
+              onChange={(e) => setReviewDescription(e.target.value)}
+            ></textarea>
+          </div>
+          <div>
+            <button type="button" className="btn btn-primary mt-3">
+              Submit Review
+            </button>
+          </div>
+        </form>
+      )}
     </div>
   );
 };
