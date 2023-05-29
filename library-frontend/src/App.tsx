0efdcc2bbd9c9ede3bc38bc54f76feae563c1d8e
Dmytro Iovenko <iovenko@gmail.com>
2023-05-28 21:53:12 -0400

    creating messages page component

--- library-frontend/src/App.tsx
+++ library-frontend/src/App.tsx
@@ -8,6 +8,7 @@ import { ReviewListPage } from "./layouts/Utils/ReviewListPage";
 import { SpinnerLoading } from "./layouts/Utils/SpinnerLoading";
 import { SecureRoutes } from "./layouts/Utils/SecureRoutes";
 import { ShelfPage } from "./layouts/ShelfPage/ShelfPage";
+import { MessagesPage } from "./layouts/MessagesPage/MessagesPage";
 import { Security, LoginCallback } from "@okta/okta-react";
 import { OktaAuth, toRelativeUrl } from "@okta/okta-auth-js";
 import oktaConfig from "./configs/oktaConfig";
@@ -41,6 +42,7 @@ export const App: React.FC = () => {
             <Route path="login/callback" element={<LoginCallback loadingElement={<SpinnerLoading />} />} />
             <Route element={<SecureRoutes />}>
               <Route path="shelf" element={<ShelfPage />} />
+              <Route path="messages" element={<MessagesPage />} />
             </Route>
           </Routes>
         </div>
