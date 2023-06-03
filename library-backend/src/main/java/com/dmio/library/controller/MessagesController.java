ba739c279439bec89ee946033328243f82fd8e62
Dmytro Iovenko <iovenko@gmail.com>
2023-06-02 23:31:14 -0400

    adding admin message controller

--- library-backend/src/main/java/com/dmio/library/controller/MessagesController.java
+++ library-backend/src/main/java/com/dmio/library/controller/MessagesController.java
@@ -5,12 +5,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dmio.library.entity.Message;
+import com.dmio.library.model.AdminMessage;
 import com.dmio.library.service.MessagesService;
 import com.dmio.library.utils.ExtractJWT;
 
@@ -26,6 +28,18 @@ public class MessagesController {
             @RequestBody Message messageRequest) {
         String userEmail = ExtractJWT.jwtExtraction(token, "sub");
         messagesService.postMessage(messageRequest, userEmail);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PutMapping("/secure/admin/message")
+    public ResponseEntity<HttpStatus> putMessage(@RequestHeader(value = "Authorization") String token,
+            @RequestBody AdminMessage adminMessage) throws Exception {
+        String userEmail = ExtractJWT.jwtExtraction(token, "sub");
+        String admin = ExtractJWT.jwtExtraction(token, "userType");
+        if (admin == null || !admin.equals("admin")) {
+            throw new Exception("Administration page only.");
+        }
+        messagesService.putMessage(adminMessage, userEmail);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
