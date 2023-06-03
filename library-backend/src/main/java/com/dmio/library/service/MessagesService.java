fcf378f078d7eca8767dec0b3988286807387e20
Dmytro Iovenko <iovenko@gmail.com>
2023-06-02 23:15:35 -0400

    adding admin message service

--- library-backend/src/main/java/com/dmio/library/service/MessagesService.java
+++ library-backend/src/main/java/com/dmio/library/service/MessagesService.java
@@ -1,5 +1,7 @@
 package com.dmio.library.service;
 
+import java.util.Optional;
+
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +9,7 @@ import org.springframework.stereotype.Service;
 
 import com.dmio.library.dao.MessageRepository;
 import com.dmio.library.entity.Message;
+import com.dmio.library.model.AdminMessage;
 
 @Service
 @Transactional
@@ -18,6 +21,17 @@ public class MessagesService {
         Message message = new Message(messageRequest.getTitle(), messageRequest.getQuestion());
         message.setUserEmail(userEmail);
         messageRepository.save(message);
+    }
+
+    public void putMessage(AdminMessage adminMessage, String userEmail) throws Exception {
+        Optional<Message> message = messageRepository.findById(adminMessage.getId());
+        if (!message.isPresent()) {
+            throw new Exception("Message not found");
+        }
+        message.get().setAdminEmail(userEmail);
+        message.get().setResponse(adminMessage.getResponse());
+        message.get().setClosed(true);
+        messageRepository.save(message.get());
     }
 
 }
