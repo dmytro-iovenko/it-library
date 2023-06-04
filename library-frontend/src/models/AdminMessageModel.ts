c7d770908f5c18cf7b5b425af45647eaa6807686
Dmytro Iovenko <iovenko@gmail.com>
2023-06-03 20:21:32 -0400

    creating admin message model

---
+++ library-frontend/src/models/AdminMessageModel.ts
@@ -0,0 +1,10 @@
+export class AdminMessageModel {
+    id: number;
+    response: string;
+
+    constructor(id: number, response: string) {
+        this.id = id;
+        this.response = response;
+    }
+}
+
